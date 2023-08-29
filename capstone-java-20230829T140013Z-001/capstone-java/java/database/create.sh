dbc1ee24c8ce47060f26c713d2cf483c08679cc2
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-08-29 13:42:23 -0500

    Change DB name

--- capstone-java-20230829T140013Z-001/capstone-java/java/database/create.sh
+++ capstone-java-20230829T140013Z-001/capstone-java/java/database/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PGPASSWORD='1234'
 BASEDIR=$(dirname $0)
-DATABASE=final_capstone
+DATABASE=rental_property
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
