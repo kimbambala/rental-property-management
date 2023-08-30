eafa3eb8c533b177ecf958563f1ffa70222aa195
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-08-30 09:36:45 -0500

    populate browse homes with dummy data from store

---
+++ capstone-java-20230829T140013Z-001/capstone-java/vue/src/components/HomeCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="houseCard">
+    <h2>Currently Available</h2>
+    <p>Bedrooms: {{ home.rooms }}</p>
+    <p>Bathrooms: {{home.bathrooms}}</p>
+    <p>${{home.rent}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "house-card",
+  props: {
+    home: Object
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+h1 {
+    text-align: center;
+}
+.houseCard {
+  border: 2px solid #333;
+  border-radius: 10px;
+  background-color: #f9f9f9;
+  width: 250px;
+  height: 200px;
+  margin: 20px;
+  display: grid;
+  bottom: 40px;
+  left: 10%;
+  font-size: 1rem;
+  place-items: center;
+  text-decoration: none;
+  transition: border 0.2s ease, transform 0.3s ease-in-out;
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+}
+.houseCard:hover {
+  border: 3px solid black;
+  transform: translateY(-5px);
+  box-shadow: 0px 10px 20px 2px rgba(0, 0, 0, 0.25);
+}</style>
--- capstone-java-20230829T140013Z-001/capstone-java/vue/src/views/Browse.vue
+++ capstone-java-20230829T140013Z-001/capstone-java/vue/src/views/Browse.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>Browse</div>
+  <div>
+    <h1>Browse</h1>
+    <div class="cards">
+      <home-card
+        v-for="home in homes"
+        :key="home.homeId"
+        :home="home"
+      ></home-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCard from "../components/HomeCard.vue";
 export default {
-
-}
+  components: {
+    HomeCard,
+  },
+  data() {
+    return {
+      homes: [],
+    };
+  },
+  methods: {
+    getHomes() {
+      this.homes = this.$store.getters.getHomes;
+    },
+  },
+  created() {
+    this.getHomes();
+  },
+};
 </script>
 
 <style>
-
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 </style>
