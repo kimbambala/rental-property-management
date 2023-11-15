6af0e44c23071de43c767d101535a886a6fe1bf1
kimbambala <nickprincy@gmail.com>
2023-11-14 17:20:17 -0800

    Worked on contact Owner page

--- vue/src/views/ContactOwnerView.vue
+++ vue/src/views/ContactOwnerView.vue
@@ -6,6 +6,19 @@
         </div>
         
       <div class="cards">
+        <div class="mb-3">
+          <div>To:</div>
+          <label for="exampleFormControlInput1" class="form-label"></label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <br>
+          <div>Message:</div>
+          <label for="exampleFormControlTextarea1" class="form-label"></label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+        <button>Send</button>
 
       </div>
     </div>
@@ -18,3 +31,31 @@
   
   }
   </script>
+<style scoped>
+
+  .cards{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+  }
+
+  input{
+    width: 500px;
+    height: 50px;
+    font-size: 17px;
+  }
+
+  textarea{
+    width: 500px;
+    height: 250px;
+    font-size: 17px;
+  }
+
+  button{
+    height: 40px;
+    cursor: pointer;
+    font-size: 25px;
+
+  }
+
+</style>
